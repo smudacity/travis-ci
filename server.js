2bdcf10c806d48e96fac7feede707596356d57c3
Sricon <sricon@gmail.com>
2022-05-02 17:57:12 +0100

    env var test

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log('Containers rule!');
+    console.log('Env variable Deal of the day:', process.env.deal_OF_THE_DAY)
     await sleep(5000);
   }
 }
